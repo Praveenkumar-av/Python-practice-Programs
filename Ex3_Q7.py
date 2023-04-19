b4f74b573560dc8b59445e4db753f99d83ebc351
Praveenkumar-av <109723900+Praveenkumar-av@users.noreply.github.com>
2023-04-19 13:19:20 +0530

    Exercise 3 updated

---
+++ Ex3_Q7.py
@@ -0,0 +1,8 @@
+  #There are 12 musical notes in chromatic scale namely
+    #C C# D D# E F F# G G# A A# B
+ #Interval between each pair of notes is called as semitone. D# is 3 semitones above C, 
+ #E is 2 semitones above D. Every note has a major scale which comprises of 7 out of 12 notes 
+ #which are 0,2,4,5,7,9 and 11 semitones above the current note.
+ #Example: For note D, major scale comprises of (D, E, F#, G, A, B,C#), For note C, major scale 
+ #comprises of (C, D, E, F, G, A, B) Write a script which accepts a note and prints its major scale.
+
